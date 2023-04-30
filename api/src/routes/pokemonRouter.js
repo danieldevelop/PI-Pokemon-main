23ec829258b7f08c833ea33ff0f8d97b6aaa7021
Daniel Gomez <dgomez.informatica@gmail.com>
2023-04-30 19:21:27 -0400

    add new ruta para encontrar pokemon por id

--- api/src/routes/pokemonRouter.js
+++ api/src/routes/pokemonRouter.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const {
     getPokemonsHandler,
+    getPokemonsByIdHandler,
 } = require("../handlers/pokemonHandler");
 
 const pokemonRouter = Router();
 
 pokemonRouter.get("/", getPokemonsHandler);
+pokemonRouter.get("/:id", getPokemonsByIdHandler);
 
 
 module.exports = pokemonRouter;
