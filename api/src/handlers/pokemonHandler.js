3fd9af61ff91ed5af1260694ae08bad3c494c865
Daniel Gomez <dgomez.informatica@gmail.com>
2023-04-30 19:23:22 -0400

    add funcion asyncrona pata obtener el id del pokemon

--- api/src/handlers/pokemonHandler.js
+++ api/src/handlers/pokemonHandler.js
@@ -1,17 +1,35 @@
 const {
     getAllPokemons,
+    getPokemonById,
 } = require("../controllers/pokemonController")
+
 
 const getPokemonsHandler = async (req, res) => {
     try {
         const pokemons = await getAllPokemons();
         res.status(200).json(pokemons);
+
     } catch (err) {
         res.status(400).json({ message: err.message });
     }
-}
+};
+
+
+const getPokemonsByIdHandler = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const response = await getPokemonById(id);
+        if (!response) return res.status(404).json({ message: "Pokemon not found" });
+        return res.status(200).json(response);
+
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
 
 
 module.exports = {
     getPokemonsHandler,
+    getPokemonsByIdHandler,
 }
