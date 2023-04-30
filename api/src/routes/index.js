0da011b7652132fc560408d7451532c31e9a5887
Daniel Gomez <dgomez.informatica@gmail.com>
2023-04-30 15:57:30 -0400

    add route to model pokemon

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const PokemonRouter = require('./pokemonRouter');
 
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.use('/pokemon', PokemonRouter);
 
 
 module.exports = router;
