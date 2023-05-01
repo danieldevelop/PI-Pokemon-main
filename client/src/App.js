e8249cbcf2dfeb043f63adbd8a00ace54021c78d
Daniel Gomez <dgomez.informatica@gmail.com>
2023-04-30 23:57:39 -0400

    add ruta para error 404

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 //import './App.css';
 
 import Landing from "./views/landing/Landing";
+import NotFound from "./views/notFound/NotFound";
 
 import { Route, Switch } from "react-router-dom";
 
@@ -9,6 +10,9 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path="/" component={Landing} />
+
+        {/* En cas de que no encuentre la página */}
+        <Route path="*" component={NotFound} />
       </Switch>
     </div>
   );
