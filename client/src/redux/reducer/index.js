644d865a390062f6ea91ffe945e075fea66dd4eb
Daniel Gomez <dgomez.informatica@gmail.com>
2023-05-01 19:42:24 -0400

    add casos de actions segun el case

---
+++ client/src/redux/reducer/index.js
@@ -0,0 +1,37 @@
+import {
+    ALL_POKEMONS,
+    BY_NAME,
+    BY_ID
+} from "../actions/actions-types";
+
+const initialState = {
+    allPokemons: [],
+    pokemonsCopy: [], // hace una copia de todos los pokemons, con esto podemos volver al estado original
+    detailPokemon: [],
+};
+
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ALL_POKEMONS:
+            return {
+                ...state,
+                allPokemons: action.payload,
+                pokemonsCopy: action.payload,
+            };
+        case BY_NAME:
+            return {
+                ...state,
+                allPokemons: action.payload,
+            };
+        case BY_ID:
+            return {
+                ...state,
+                detailPokemon: action.payload,
+            };
+        default:
+            return state;
+    }
+};
+
+
+export default rootReducer;
