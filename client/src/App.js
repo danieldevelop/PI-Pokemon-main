471d1d445a8dfda1e57d7a72e9098b0f432fc2a6
Daniel Gomez <dgomez.informatica@gmail.com>
2023-05-04 00:47:13 -0400

    nueva ruta para el detalle del pokemon

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@
 import Landing from "./views/landing/Landing";
 import NotFound from "./views/notFound/NotFound";
 import Home from "./views/home/Home";
+import Detail from "./views/detail/Detail";
 
 import { Route, Switch } from "react-router-dom";
 
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/home" component={Home} />
+        <Route path="/detail/:id" component={Detail} />
 
         {/* En cas de que no encuentre la página */}
         <Route path="*" component={NotFound} />
